8b00e8a8a51542a8869802abb5ade6cd3e6cc08c
Jose Manuel <jose.galonso.2010@gmail.com>
2024-11-29 19:39:49 +0100

    primerCommit

--- src/main/java/Exercise3/exercise3.java
+++ src/main/java/Exercise3/exercise3.java
@@ -1,4 +1,14 @@
 package Exercise3;
 
 public class exercise3 {
+
+    public static void main(String[] args) {
+        /*
+        *Write a program that reads several lines of text consisting only of integers.
+        *The integers in each line should be stored in an object of type List
+        * (integers are separated by spaces), and this representation of each line should be stored
+        * in an object of type HashSet. Then the program should print out the contents of this set
+        * using forEach method for the object of type List, and the forEach for the object of type HashSet.
+        */
+    }
 }
