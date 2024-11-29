ba71c2cb48171d4264508d017aefc561baf5d0f5
Jose Manuel <jose.galonso.2010@gmail.com>
2024-11-29 19:52:39 +0100

    Ejercicio3

--- src/main/java/Exercise3/exercise3.java
+++ src/main/java/Exercise3/exercise3.java
@@ -1,33 +1,42 @@
 package Exercise3;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class exercise3 {
 
     public static void main(String[] args) {
         /*
-        *Write a program that reads several lines of text consisting only of integers.
-        *The integers in each line should be stored in an object of type List
-        * (integers are separated by spaces), and this representation of each line should be stored
-        * in an object of type HashSet. Then the program should print out the contents of this set
-        * using forEach method for the object of type List, and the forEach for the object of type HashSet.
-        */
+         *Write a program that reads several lines of text consisting only of integers.
+         *The integers in each line should be stored in an object of type List
+         * (integers are separated by spaces), and this representation of each line should be stored
+         * in an object of type HashSet. Then the program should print out the contents of this set
+         * using forEach method for the object of type List, and the forEach for the object of type HashSet.
+         */
 
         //variables
 
         Scanner sc = new Scanner(System.in);
-
+        List<Integer> list = new ArrayList<Integer>();
         Set<List<Integer>> hashSet = new HashSet<>();
 
 
         System.out.println("Introduce some words");
-        while(true){
-            String word = sc.nextLine().trim();
-            if(word.isEmpty()) break;
+        String word;
+        while (true) {
+            word = sc.nextLine().trim();
+            if (word.isEmpty()) {
+                break;
+            }
         }
+
+
+        //Convert the words on Integers
+        String[] numbers = word.split(" ");
+        for (String number : numbers) {
+            list.add(Integer.parseInt(number));
+        }
+
+        hashSet.add(list);
 
     }
 }
