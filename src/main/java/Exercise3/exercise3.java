860d5c5a8c079893e86d49f038c84513980d65cd
Jose Manuel <jose.galonso.2010@gmail.com>
2024-11-29 19:47:49 +0100

    Ejercicio3

--- src/main/java/Exercise3/exercise3.java
+++ src/main/java/Exercise3/exercise3.java
@@ -1,5 +1,10 @@
 package Exercise3;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
 public class exercise3 {
 
     public static void main(String[] args) {
@@ -10,5 +15,19 @@ public class exercise3 {
         * in an object of type HashSet. Then the program should print out the contents of this set
         * using forEach method for the object of type List, and the forEach for the object of type HashSet.
         */
+
+        //variables
+
+        Scanner sc = new Scanner(System.in);
+
+        Set<List<Integer>> hashSet = new HashSet<>();
+
+
+        System.out.println("Introduce some words");
+        while(true){
+            String word = sc.nextLine().trim();
+            if(word.isEmpty()) break;
+        }
+
     }
 }
